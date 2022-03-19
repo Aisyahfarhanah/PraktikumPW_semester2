f42b55d871a441a343183ac29861041672a857d2
Aisyahfarhanah <100469029+Aisyahfarhanah@users.noreply.github.com>
2022-03-19 08:27:59 +0700

    Add files via upload

---
+++ praktikum3/startbootstrap-sb-admin-gh-pages/view/artikel2.php
@@ -0,0 +1,24 @@
+<?php
+include '../layout/header.php';
+?>
+
+<h1>Welcome To Website</h1>
+<p>ini adalah artikel 2</p>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<h2>
+				Heading
+			</h2>
+			<p>
+				Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+			</p>
+			<p>
+				<a class="btn" href="#">View details »</a>
+			</p>
+		</div>
+	</div>
+</div>
+<?php
+include '../layout/footer.php';
+?>

cfe554a71fe9e8299720c3e5022600552ca7c73e
Aisyahfarhanah <100469029+Aisyahfarhanah@users.noreply.github.com>
2022-05-14 15:01:23 +0700

    Delete praktikum8 directory

--- praktikum8/application/models/Dosen_model.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//buat Class
-class Dosen_model extends CI_Model {
-    //buat struktur data property/variabel
-    public $id, $nama, $nidn, $gender, $tmp_lahir, $tgl_lahir, $pendidikan;
-}
-?>
